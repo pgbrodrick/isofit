4829f882aed72432fa0c1c957a33f253892c843f
pgbrodrick <phil.brodrick@gmail.com>
2022-01-01 17:43:21 -0800

    triangular grid

--- isofit/core/common.py
+++ isofit/core/common.py
@@ -23,7 +23,7 @@ import json
 import xxhash
 import numpy as np
 import scipy.linalg
-from scipy.interpolate import RegularGridInterpolator
+from scipy.interpolate import RegularGridInterpolator, griddata, LinearNDInterpolator, CloughTocher2DInterpolator
 from os.path import expandvars, split, abspath
 from typing import List
 from collections import OrderedDict
@@ -37,7 +37,7 @@ eps = 1e-5
 
 ### Classes ###
 
-class VectorInterpolatorOld:
+class VectorInterpolatorOrig:
     """ Linear look up table interpolator.  Support linear interpolation through radial space by expanding the look
         up tables with sin and cos dimensions.
 
@@ -122,7 +122,7 @@ class VectorInterpolatorOld:
         self.n = data.shape[-1]
         grid_aug = grid + [np.arange(data.shape[-1])]
         self.itp = RegularGridInterpolator(grid_aug, data,
-                                           bounds_error=False, fill_value=None)
+                                           bounds_error=False, fill_value=None, method='linear')
 
     def __call__(self, points):
 
@@ -154,7 +154,135 @@ class VectorInterpolatorOld:
 
         return res
 
+
 class VectorInterpolator:
+    """ Linear look up table interpolator.  Support linear interpolation through radial space by expanding the look
+        up tables with sin and cos dimensions.
+
+        Args:
+            grid_input: list of lists of floats, indicating the gridpoint elements in each grid dimension
+            data_input: n dimensional array of radiative transfer engine outputs (each dimension size corresponds to the
+                        given grid_input list length, with the last dimensions equal to the number of sensor channels)
+            lut_interp_types: a list indicating if each dimension is in radiance (r), degrees (r), or normal (n) units.
+        """
+
+    def __init__(self, grid_input: List[List[float]], data_input: np.array, lut_interp_types: List[str]):
+        self.lut_interp_types = lut_interp_types
+        self.single_point_data = None
+
+        # Lists and arrays are mutable, so copy first
+        grid = grid_input.copy()
+        data = data_input.copy()
+
+        # Check if we are using a single grid point. If so, store the grid input.
+        if np.prod(list(map(len, grid))) == 1:
+            self.single_point_data = data
+
+        # expand grid dimensionality as needed
+        [radian_locations] = np.where(self.lut_interp_types == 'd')
+        [degree_locations] = np.where(self.lut_interp_types == 'r')
+        angle_locations = np.hstack([radian_locations, degree_locations])
+        angle_types = np.hstack(
+            [self.lut_interp_types[radian_locations],
+             self.lut_interp_types[degree_locations]])
+        for _angle_loc in range(len(angle_locations)):
+
+            angle_loc = angle_locations[_angle_loc]
+            # get original grid at given location
+            original_grid_subset = np.array(grid[angle_loc])
+
+            # convert for angular coordinates
+            if (angle_types[_angle_loc] == 'r'):
+                grid_subset_cosin = np.cos(original_grid_subset)
+                grid_subset_sin = np.sin(original_grid_subset)
+            elif (angle_types[_angle_loc] == 'd'):
+                grid_subset_cosin = np.cos(original_grid_subset / 180. * np.pi)
+                grid_subset_sin = np.sin(original_grid_subset / 180. * np.pi)
+
+            # handle the fact that the grid may no longer be in order
+            grid_subset_cosin_order = np.argsort(grid_subset_cosin)
+            grid_subset_sin_order = np.argsort(grid_subset_sin)
+
+            # convert current grid location, and add a second
+            grid[angle_loc] = grid_subset_cosin[grid_subset_cosin_order]
+            grid.insert(angle_loc + 1, grid_subset_sin[grid_subset_sin_order])
+
+            # now copy the data to be interpolated through the extra dimension,
+            # at the specific angle_loc axes.  We'll use broadcast_to to do
+            # this, but we need to do it on the last dimension.  So start by
+            # temporarily moving the target axes there, then broadcasting
+            data = np.swapaxes(data, -1, angle_loc)
+            data_dim = list(np.shape(data))
+            data_dim.append(data_dim[-1])
+            data = data[..., np.newaxis] * np.ones(data_dim)
+
+            # Now we need to actually copy the data between the first two axes,
+            # as broadcast_to doesn't do this
+            for ind in range(data.shape[-1]):
+                data[..., ind] = data[..., :, ind]
+
+            # Now re-order the cosin dimension
+            data = data[..., grid_subset_cosin_order, :]
+            # Now re-order the sin dimension
+            data = data[..., grid_subset_sin_order]
+
+            # now re-arrange the axes so they're in the right order again,
+            dst_axes = np.arange(len(data.shape) - 2).tolist()
+            dst_axes.insert(angle_loc, len(data.shape) - 2)
+            dst_axes.insert(angle_loc + 1, len(data.shape) - 1)
+            dst_axes.remove(angle_loc)
+            dst_axes.append(angle_loc)
+            data = np.ascontiguousarray(np.transpose(data, axes=dst_axes))
+
+            # update the rest of the angle locations
+            angle_locations += 1
+
+        self.n = data.shape[-1]
+        grid_aug = grid + [np.arange(data.shape[-1])]
+        #self.grid = tuple([np.array(x) for x in grid])
+        self.grid = np.stack(np.meshgrid(*grid, indexing='ij'),axis=-1)
+        self.data = data
+
+        self.grid = np.reshape(self.grid, (int(np.product(self.grid.shape)/self.grid.shape[-1]), self.grid.shape[-1]))
+        self.data = np.reshape(self.data, (int(np.product(self.data.shape)/self.data.shape[-1]), self.data.shape[-1]))
+
+        self.itp = LinearNDInterpolator(self.grid, self.data, rescale=False)
+        #self.itp = CloughTocher2DInterpolator(self.grid, self.data)
+
+        #self.itp = RegularGridInterpolator(grid_aug, data,
+        #                                   bounds_error=False, fill_value=None)
+
+    def __call__(self, points):
+
+        # If we only have one point, we can't do any interpolation, so just
+        # return the original data.
+        if self.single_point_data is not None:
+            return self.single_point_data
+
+        x = np.zeros((len(points) +
+                      np.sum(self.lut_interp_types != 'n')))
+        offset_count = 0
+        for i in range(len(points)):
+            if self.lut_interp_types[i] == 'n':
+                x[i + offset_count] = points[i]
+            elif self.lut_interp_types[i] == 'r':
+                x[i + offset_count] = np.cos(points[i])
+                x[i + 1 + offset_count] = np.sin(points[i])
+                offset_count += 1
+            elif self.lut_interp_types[i] == 'd':
+                x[i + offset_count] = np.cos(points[i] / 180. * np.pi)
+                x[i + 1 + offset_count] = np.sin(points[i] / 180. * np.pi)
+                offset_count += 1
+
+        #import ipdb; ipdb.set_trace()
+        #res = griddata(self.grid, self.data, x, method='linear')
+        #res = griddata(self.grid, self.data, x, method='cubic', rescale=False)
+        res = self.itp(x)
+
+        return res
+
+
+class VectorInterpolatorJouni:
     """Multilinear interpolation for even regular grids, which we
     expect to be using. The arguments are the following:
 
